b4232f7994e213c1597f14890455dd8f8fbebac1
morrna <morrna@gmail.com>
2017-05-09 00:08:22 -0600

    lib.hs: successful comparison function, also full range of guesses

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,10 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = putStrLn "Not there yet"
+
+numPegs = 4
+numColors = 6
+
+properPeg' = (properPeg numColors)
+properGuess' = (properGuess numPegs numColors)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,36 @@
 module Lib
-    ( someFunc
+    ( properPeg
+    , properGuess
+    , guessComp
+    , allGuesses
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "The app will have to wait."
+properPeg :: Int -> Int -> Bool
+properPeg numColors p = (p >= 0) && (p < numColors)
+
+properGuess :: Int -> Int -> [Int] -> Bool
+properGuess numPegs numColors g = (length g == numPegs)
+                && (all (properPeg numColors) g )
+
+intEq :: Int -> Int -> Int
+intEq i j
+    | i == j = 1
+    | i /= j = 0
+
+guessComp :: [Int] -> [Int] -> (Int, Int)
+guessComp g h
+    | length g /= length h = error "Guesses must be the same length!"
+guessComp g h = (sum $ zipWith intEq g h
+                , sum [ (g !! i) `intEq` (h !! j)
+                        | i <- [0 .. (length g - 1)]
+                        , j <- [0 .. (length h - 1)]
+                        , i /= j
+                        , (g !! i) /= (h !! i)
+                        , (g !! j) /= (h !! j)
+                    ]
+                )
+
+allGuesses :: Int -> Int -> [[Int]]
+allGuesses 0 _ = [[]]
+allGuesses np nc = [ c:ps | c <- [0 .. (nc - 1)]
+                            , ps <- (allGuesses (np-1) nc) ]
