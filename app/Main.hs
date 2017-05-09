d9d46320bf72b9e2028a5696f107d4bcb79f994a
morrna <morrna@gmail.com>
2017-05-09 00:50:57 -0600

    Basic working version; binary added to repo.
    Main.hs: numColors changed to match Ubuntu mastermind game.

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,43 @@ module Main where
 
 import Lib
 
-main :: IO ()
-main = putStrLn "Not there yet"
+main = do
+    let numPegs = 4
+    let numColors = 8
+    let properPeg' = (properPeg numColors)
+    let properGuess' = (properGuess numPegs numColors)
+    let allG = allGuesses numPegs numColors
 
-numPegs = 4
-numColors = 6
+    let firstGuess = map (`mod` numColors) [ 0 .. (numPegs-1) ]
 
-properPeg' = (properPeg numColors)
-properGuess' = (properGuess numPegs numColors)
+    putStrLn "starting Mastermind solver"
+    putStrLn $ "This is for a game with " ++ (show numPegs)
+            ++ " pegs to guess and " ++ (show numColors)
+            ++ " distinct colors."
+
+    win <- narrowGuesses firstGuess allG
+    if win
+        then putStrLn "Congrats on the win!"
+        else putStrLn "Aw, what went wrong?"
+
+
+
+
+-- Input is a guess; output is an IO wrapped version of remaining possibilities
+narrowGuesses :: [Int] -> [[Int]] -> (IO Bool)
+narrowGuesses tryG oldposs = do
+    putStrLn $ "Try this guess: " ++ (show tryG)
+    putStrLn "How many red pegs did you get?"
+    redStr <- getLine
+    putStrLn "How many white pegs did you get?"
+    whiteStr <- getLine
+    let comp = ( (read redStr :: Int), (read whiteStr :: Int) )
+    if comp == (4,0)
+        then do
+            putStrLn $ "There were " ++ (show $ length oldposs)
+                    ++ " possibilities left."
+            return True
+        else do
+            let newposs = filter (\gg -> guessComp tryG gg == comp) oldposs
+            narrowGuesses (head newposs) newposs
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -5,6 +5,8 @@ module Lib
     , allGuesses
     ) where
 
+import Data.List
+
 properPeg :: Int -> Int -> Bool
 properPeg numColors p = (p >= 0) && (p < numColors)
 
@@ -12,25 +14,39 @@ properGuess :: Int -> Int -> [Int] -> Bool
 properGuess numPegs numColors g = (length g == numPegs)
                 && (all (properPeg numColors) g )
 
-intEq :: Int -> Int -> Int
-intEq i j
-    | i == j = 1
-    | i /= j = 0
-
-guessComp :: [Int] -> [Int] -> (Int, Int)
-guessComp g h
-    | length g /= length h = error "Guesses must be the same length!"
-guessComp g h = (sum $ zipWith intEq g h
-                , sum [ (g !! i) `intEq` (h !! j)
-                        | i <- [0 .. (length g - 1)]
-                        , j <- [0 .. (length h - 1)]
-                        , i /= j
-                        , (g !! i) /= (h !! i)
-                        , (g !! j) /= (h !! j)
-                    ]
-                )
-
 allGuesses :: Int -> Int -> [[Int]]
 allGuesses 0 _ = [[]]
 allGuesses np nc = [ c:ps | c <- [0 .. (nc - 1)]
                             , ps <- (allGuesses (np-1) nc) ]
+
+-- Utility stuff
+-- probably need this because I don't know the libraries well enough.
+countIn :: [Int] -> Int -> Int
+countIn l v = length $ filter (v==) l
+
+guessComp :: [Int] -> [Int] -> (Int, Int)
+guessComp = curry $ guessesInexact . (uncurry guessesExact)
+--guessComp h g = guessesInexact $ guessesExact h g
+
+-- Takes two lists and returns the number that exactly match,
+-- along with the elements left over.
+guessesExact :: [Int] -> [Int] -> ([Int], [Int], Int)
+guessesExact h g
+    | (length h) /= (length g) = error "Compared guesses must have equal length."
+guessesExact [] [] = ( [], [], 0 )
+guessesExact (hh:ht) (gh:gt)
+    | hh == gh = ( hl
+                    , gl
+                    , en + 1 )
+    | otherwise = ( hh:hl
+                    , gh:gl
+                    , en )
+    where ( hl, gl, en ) = guessesExact ht gt
+
+-- WRONG
+-- if h has two pegs the same, a single match in g will be double counted
+guessesInexact :: ([Int], [Int], Int) -> (Int, Int)
+guessesInexact (h, g, e) = let hn = nub h in
+                           (e, sum $ zipWith min
+                            (map (countIn g) hn) (map (countIn h) hn)
+                           )
