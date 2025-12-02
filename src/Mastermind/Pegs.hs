7781d886246ca127ffdc18c267060a7c4fa0cfc2
Nathan Morrison <me@nathanmorrison.net>
2025-12-02 16:08:23 +0000

    model: Create Feedback representation with monoid instance

--- src/Mastermind/Pegs.hs
+++ src/Mastermind/Pegs.hs
@@ -5,6 +5,7 @@ Description:    Represent peg states in Mastermind
 module Mastermind.Pegs (
     Peg(..),
     allStates,
+    Feedback(..),
 ) where
 
 -- | Represent a peg as a number specifying its color.
@@ -24,3 +25,21 @@ allStates nColor nPeg
         otherPegs <- allStates nColor (nPeg-1),
         color <- [1..nColor]
     ]
+
+-- | Represents the red and white pegs used to give feedback on a guess
+data Feedback
+    = Feedback {
+        reds :: Int, -- ^ # pegs matching color and position
+        whites :: Int -- ^ # pegs matching color but not position
+    }
+    -- | Since the number of colors is specified at runtime, the compiler needs something in case list lengths mismatch.
+    | InvalidFeedback
+    deriving (Show, Eq)
+
+instance Semigroup Feedback where
+    (Feedback r1 w1) <> (Feedback r2 w2) = Feedback (r1+r2) (w1+w2)
+    InvalidFeedback <> _ = InvalidFeedback
+    _ <> InvalidFeedback = InvalidFeedback
+
+instance Monoid Feedback where
+    mempty = Feedback 0 0
